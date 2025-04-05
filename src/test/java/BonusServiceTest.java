12b9747a51ee4b4246bd717b9aebb9b6fa92366b
unknown <alina.yaroslavovna@gmail.com>
2025-04-05 19:47:45 +0300

    pom.xml

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -7,15 +7,12 @@ public class BonusServiceTest {
     void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
         long amount = 1000;
         boolean registered = true;
         long expected = 30;
 
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
 
@@ -23,47 +20,51 @@ public class BonusServiceTest {
     void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
         long amount = 1_000_000;
         boolean registered = true;
         long expected = 500;
 
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void shouldCalculateForUnregisteredAndUnderLimit() {
         BonusService service = new BonusService();
+
         long amount = 1000;
         boolean registered = false;
-        long expected = 10; // 1% от 1000
+        long expected = 10;
 
         long actual = service.calculate(amount, registered);
+
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void shouldCalculateForUnregisteredAndOverLimit() {
         BonusService service = new BonusService();
+
         long amount = 1_000_000;
         boolean registered = false;
         long expected = 500;
+
         long actual = service.calculate(amount, registered);
+
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void shouldReturnZeroForAmountZero() {
+    void shouldReturnZeroForRegisteredUserWithZeroAmount() {
         BonusService service = new BonusService();
+
         long amount = 0;
-        boolean registered = true; // или false, результат должен быть одинаковым
+        boolean registered = true;
         long expected = 0;
 
         long actual = service.calculate(amount, registered);
+
         Assertions.assertEquals(expected, actual);
     }
 }
